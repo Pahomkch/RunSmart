8d62a1a54959e75793581f3a75c262b71674e7b5
Andrew Pahomov <Pahomov.kch@gmail.com>
2020-01-13 00:01:24 +0500

    Add PopUp and adaptiv to mobile that

--- src/js/script.js
+++ src/js/script.js
@@ -2,27 +2,27 @@
 /* slick - slider на JQuery */
 
 $(document).ready(function(){
-		$('.corusel__inner').slick({
-			infinite: true,
-			speed: 1300,
-			autoplay: true,
-			autoplaySpeed: 2000,
-			arrows: true,
-			prevArrow: '<button type="button" class="prev"></button>',
-			nextArrow: '<button type="button" class="next"></button>',
-			adaptiveHeight: false,
-			responsive: [
-				{
-					breakpoint: 992,
-					settings: {
-						arrows: false,
-						dots: true,
-					}
+	$('.corusel__inner').slick({
+		infinite: true,
+		speed: 1300,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: true,
+		prevArrow: '<button type="button" class="prev"></button>',
+		nextArrow: '<button type="button" class="next"></button>',
+		adaptiveHeight: false,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					dots: true,
 				}
-			],
-		})
-	}
-);
+			}
+		],
+	})
+	
+});
 
 $(document).ready(function(){
   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
@@ -52,40 +52,74 @@ $(document).ready(function(){
   }); 
   */
 
-  function toggleSlide(item) {
-    $(item).each(function(i) {
-        $(this).on('click', function(e) {
-            e.preventDefault();
-            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        })
-    });
-};
+	function toggleSlide(item) {
+		$(item).each(function(i) {
+			$(this).on('click', function(e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			})
+		});
+	};
 
-toggleSlide('.catalog-item__link');
-toggleSlide('.catalog-item__back');
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
 
+
+
+
+							/* 
+							tiny slider with rpev and next buttons
+							const slider = tns({
+								container: '.corusel__inner',
+								items: 1,
+								slideBy: 'page',
+								autoplay: true,
+								nav: false,
+								controls: false,
+								autoplay: false,
+							});
+
+							document.querySelector('.prev').addEventListener('click', function () {
+								slider.goTo('prev');
+							}); 
+
+							document.querySelector('.next').addEventListener('click', function () {
+								slider.goTo('next');
+							}); 
+							*/
+
+/*****************  popUp modal *******************/
+	function modal_close(modal) {
+		$('.modal__close').on('click', function() {
+			$('.overlay').fadeOut('slow')
+			$(modal).fadeOut('slow')
+		});
+	};
+
+	//take consultation PopUp
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn('slow')
+	});
+	modal_close('#consultation');
+
+	/*take order PopUp *********************/  
+
+
+/* 	$('[data-modal=order]').on('click', function() {
+		$('.overlay, #order').fadeIn('slow')
+	}); */
+
+	$('.button_mini').each(function(i){
+		$(this).on('click', function() {
+			$('#order .modal-order__description').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		})
+	});
+	
+	modal_close('#order');
+	
+
+
+	
 });
-
-
-/* 
-tiny slider with rpev and next buttons
-const slider = tns({
-	container: '.corusel__inner',
-	items: 1,
-	slideBy: 'page',
-	autoplay: true,
-	nav: false,
-	controls: false,
-	autoplay: false,
-});
-
-document.querySelector('.prev').addEventListener('click', function () {
-	slider.goTo('prev');
-}); 
-
-document.querySelector('.next').addEventListener('click', function () {
-	slider.goTo('next');
-}); 
-*/
-
